e742f47564dbbf80887a6f15746e4c84beaf743e
Evgeniya <tss.tish.tssgmail.com>
2025-10-08 14:49:54 +0300

    Prepare for production: PostgreSQL, security settings, deployment config

---
+++ build.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+# build.sh
+
+echo "Installing dependencies..."
+pip install -r requirements.txt
+
+echo "Running migrations..."
+python manage.py makemigrations
+python manage.py migrate
+
+echo "Collecting static files..."
+python manage.py collectstatic --noinput --clear
+
+echo "Build completed!"
